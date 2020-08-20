e48d5391b1a39c30bff85fe18b57e1b2699ff697
Alexey Arguchinsky <arguchinsky55@gmail.com>
2020-08-20 09:03:03 +0300

    array task4 has done

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ import { task15Old, task15New } from './es-features/task15';
 import { any } from './array-practice/task1';
 import { arrayDiff } from './array-practice/task2';
 import { forEachRight } from './array-practice/task3';
+import { union } from './array-practice/task4';
 
 console.log('task1 old', task1Old(10) === 500); // true
 console.log('task1 new', task1New(10) === 500); // true
@@ -129,7 +130,8 @@ console.log('====== task 3 start =======');
 forEachRight([1, 2, 3, 4], val => console.log(val)); // в консоль 4 3 2 1
 console.log('======  task 3 end  =======');
 console.log('====== task 4 start =======');
-
+console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); // [5, 1, 2, 3, 4]
+console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); // [5, 1, 3, 4]
 console.log('======  task 4 end  =======');
 console.log('====== task 5 start =======');
 
